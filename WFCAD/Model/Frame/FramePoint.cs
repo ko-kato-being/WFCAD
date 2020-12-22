54d2ce5a3bd7106dac7bbe79ae53f4ca0fb62e59
加藤孝規 <ko-kato@beingcorp.co.jp>
2020-12-22 16:17:12 +0900

    枠の点クラスを追加

---
+++ WFCAD/Model/Frame/FramePoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace WFCAD.Model.Frame {
+    /// <summary>
+    /// 枠の点クラス
+    /// </summary>
+    public class FramePoint : IFramePoint {
+
+        #region プロパティ
+
+        /// <summary>
+        /// 座標
+        /// </summary>
+        public Point Point { get; set; }
+
+        /// <summary>
+        /// 基準点
+        /// </summary>
+        public List<Point> BasePoints { get; set; }
+
+        /// <summary>
+        /// 選択されているか
+        /// </summary>
+        public bool IsSelected { get; set; }
+
+        #endregion プロパティ
+
+        #region メソッド
+
+        /// <summary>
+        /// 描画します。
+        /// </summary>
+        public void Draw() {
+
+        }
+
+        /// <summary>
+        /// 指定した座標が円内に存在するか
+        /// </summary>
+        public bool IsHit(Point vCoordinate) {
+            return false;
+        }
+
+        #endregion メソッド
+
+    }
+}
---
+++ WFCAD/Model/Frame/IFramePoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace WFCAD.Model.Frame {
+    /// <summary>
+    /// 枠の点を表すインターフェース
+    /// </summary>
+    public interface IFramePoint {
+
+        #region プロパティ
+
+        /// <summary>
+        /// 座標
+        /// </summary>
+        Point Point { get; set; }
+
+        /// <summary>
+        /// 基準点
+        /// </summary>
+        List<Point> BasePoints { get; set; }
+
+        /// <summary>
+        /// 選択されているか
+        /// </summary>
+        bool IsSelected { get; set; }
+
+        #endregion プロパティ
+
+        #region メソッド
+
+        /// <summary>
+        /// 描画します。
+        /// </summary>
+        void Draw();
+
+        /// <summary>
+        /// 指定した座標が円内に存在するか
+        /// </summary>
+        bool IsHit(Point vCoordinate);
+
+        #endregion メソッド
+
+    }
+}
