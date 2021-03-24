b5766cbf25d352c251df94a40adbddd100c21db7
加藤孝規 <ko-kato@beingcorp.co.jp>
2021-03-24 19:41:36 +0900

    名前変え忘れ

--- WFCAD/Test/Model/CanvasTest.cs
+++ WFCAD/Test/Model/CanvasTest.cs
@@ -19,9 +19,9 @@ namespace WFCAD.Test.Model {
         }
 
         private void SelectTest(bool vIsMultiple, List<(IShape Shape, bool Result)> vTestDatas) {
-            var wShapes = new Canvas();
-            vTestDatas.ForEach(x => wShapes.Add(x.Shape));
-            wShapes.Select(new Point(), vIsMultiple);
+            var wCanvas = new Canvas(new Bitmap(1, 1));
+            vTestDatas.ForEach(x => wCanvas.Add(x.Shape));
+            wCanvas.Select(new Point(), vIsMultiple);
             for (int i = 0; i < vTestDatas.Count; i++) {
                 Assert.AreEqual(vTestDatas[i].Result, vTestDatas[i].Shape.IsSelected, message: $"図形{i + 1}");
             }
